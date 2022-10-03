9940cde797035d58e9467e03b4ae682ee32920d9
ruiji@usc.edu <ruiji@usc.edu>
2022-10-02 18:58:19 -0700

    solve the inconsistent output by putting synchronized class inside if statement

--- ruiji_CSCI201_Assignment2/src/ruiji_CSCI201_Assignment2/Operations.java
+++ ruiji_CSCI201_Assignment2/src/ruiji_CSCI201_Assignment2/Operations.java
@@ -7,27 +7,21 @@ import ruiji_CSCI201_Assignment2.SettleDate;
 public class Operations extends Thread {
 	private Semaphore semaphore;
 	private String ticker;
-	private int quantity;
-	private List<Integer> current_balance;
-	private int stock_price;
 	private int getTime;
 	private long time;
 	private boolean FinalTrans;
+	private BalanceUpdate bu;
+	private int quantity_stock;
+	
 	
 	public Operations(int Broker_num, String ticker, int quantity, List<Integer> current_balance, int stock_price, int getTime, long time, boolean FinalTrans) {
 		this.semaphore = new Semaphore(Broker_num);
 		this.ticker = ticker;
-		this.quantity = quantity;
-		this.current_balance = current_balance;
-		this.stock_price = stock_price;
 		this.getTime = getTime;
 		this.time = time;
 		this.FinalTrans = FinalTrans;
-	}
-	
-	public synchronized int setBalance(int amount) {
-		current_balance.set(0, current_balance.get(0) - amount);
-		return current_balance.get(0);
+		this.quantity_stock = quantity;
+		bu = new BalanceUpdate(current_balance, stock_price, quantity);
 	}
 	
 	public void run() {
@@ -37,54 +31,57 @@ public class Operations extends Thread {
 			Thread.sleep(getTime*1000);                                                 //sleep for 2 seconds first before trade                                                                   //set up the right now time. 
 			
 			//selling processes started, and the thread will sleep for 3 seconds to finish his job. 
-			if(quantity < 0) { 
+			if(quantity_stock < 0) { 
 				long current = System.currentTimeMillis();
 				SettleDate timestamp = new SettleDate(current - time); 
-				if(quantity == -1) System.out.println("[" + timestamp.getTime() + "]" +  "Start to sale of " + quantity * -1 + " stock of " + ticker);
+				if(quantity_stock == -1) System.out.println("[" + timestamp.getTime() + "]" +  "Start to sale of " + quantity_stock * -1 + " stock of " + ticker);
 				else
-					System.out.println("[" + timestamp.getTime() + "]" + "Start to sale of " + quantity * -1 + " stocks of " + ticker);
+					System.out.println("[" + timestamp.getTime() + "]" + "Start to sale of " + quantity_stock * -1 + " stocks of " + ticker);
 				
 				//take 3 seconds to complete the job. 
 				Thread.sleep(3000);
-				int now_balance = setBalance(quantity * stock_price);
+				
 				current = System.currentTimeMillis();
 				timestamp = new SettleDate(current - time);
-				if(quantity == -1) {
-					System.out.println("[" + timestamp.getTime() + "]" + "Finish sale of " + quantity * -1 + " stock of " + ticker);
-					System.out.println("Current Balance after trade: " + now_balance);
+				//we have to implement the synchronized class inside the if statement cause if statement may cost time that making the overwritten happened. 
+				if(quantity_stock == -1) {
+					int current_b = bu.updateBalance();                                  
+					System.out.println("[" + timestamp.getTime() + "]" + "Finish sale of " + quantity_stock * -1 + " stock of " + ticker);
+					System.out.println("Current Balance after trade: " + current_b);
 				}
 				else {
-					System.out.println("[" + timestamp.getTime() + "]" + "Finish sale of " + quantity * -1 + " stocks of " + ticker);
-					System.out.println("Current Balance after trade: " + now_balance);
+					int current_b = bu.updateBalance();
+					System.out.println("[" + timestamp.getTime() + "]" + "Finish sale of " + quantity_stock * -1 + " stocks of " + ticker);
+					System.out.println("Current Balance after trade: " + current_b);
 				}
 			}
 			//Buying processes started, and the thread will sleep for 2 seconds to finish his job. 
 			else {                                                             
 				long current = System.currentTimeMillis();
 				SettleDate timestamp = new SettleDate(current - time);
-				if(quantity == 1) System.out.println("[" + timestamp.getTime() + "]" + "Start to purchase of " + quantity + " stock of " + ticker);
+				if(quantity_stock == 1) System.out.println("[" + timestamp.getTime() + "]" + "Start to purchase of " + quantity_stock + " stock of " + ticker);
 				else
-					System.out.println("[" + timestamp.getTime() + "]" + "Start to purchase of " + quantity + " stocks of " + ticker);
+					System.out.println("[" + timestamp.getTime() + "]" + "Start to purchase of " + quantity_stock + " stocks of " + ticker);
 				
 				Thread.sleep(2000);
-				if(current_balance.get(0).compareTo(quantity * stock_price) < 0) {           
+				if(bu.canTransact() == false) {           
 					//we don't have that much balance, transaction failed
-					int now_balance = setBalance(0);
 					System.out.println("Transaction failed due to insufficient balance. Unsuccessful purchase\r\n"
-							+ "of " + quantity +  " stocks of " + ticker);
-					System.out.println("Current Balance after trade: " + now_balance);
+							+ "of " + quantity_stock +  " stocks of " + ticker);
+					System.out.println("Current Balance after trade: " + bu.getNowBalance());
 				}
 				else {
-					int now_balance = setBalance(quantity * stock_price);
 					current = System.currentTimeMillis();
 					timestamp = new SettleDate(current - time);
-					if(quantity == 1) {
-						System.out.println("[" + timestamp.getTime() + "]" + " Finish buying of " + quantity + " stock of " + ticker);
-						System.out.println("Current Balance after trade: " + now_balance);
+					if(quantity_stock == 1) {
+						int current_b = bu.updateBalance();
+						System.out.println("[" + timestamp.getTime() + "]" + " Finish buying of " + quantity_stock + " stock of " + ticker);
+						System.out.println("Current Balance after trade: " + current_b);
 					}
 					else {
-						System.out.println("[" + timestamp.getTime() + "]" + "Finish buying of " + quantity + " stocks of " + ticker);
-						System.out.println("Current Balance after trade: " + now_balance);
+						int current_b = bu.updateBalance();
+						System.out.println("[" + timestamp.getTime() + "]" + "Finish buying of " + quantity_stock + " stocks of " + ticker);
+						System.out.println("Current Balance after trade: " + current_b);
 					}
 				}
 			}
@@ -97,5 +94,32 @@ public class Operations extends Thread {
 		}finally {
 			semaphore.release();
 		}
+	}
+}
+
+class BalanceUpdate{                                     //the class interface where we implement the synchronized monitors. 
+	private List<Integer> current_balance;
+	private int quantity;
+	private int stock_price;
+	
+	public BalanceUpdate(List<Integer> current_balance, int quantity, int stock_price) {
+		this.current_balance = current_balance;
+		this.stock_price = stock_price;
+		this.quantity = quantity;
+	}
+	
+	public synchronized int updateBalance() {
+		int update_num = quantity * stock_price;
+		current_balance.set(0, current_balance.get(0) - update_num);
+		return current_balance.get(0);
+	}
+	
+	public synchronized boolean canTransact() {
+		if(current_balance.get(0).compareTo(quantity * stock_price) < 0) return false;
+		return true;
+	}
+	
+	public int getNowBalance() {
+		return current_balance.get(0);
 	}
 }
